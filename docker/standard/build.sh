6f51636f5514f57c7c639d3a552d30850fe8313e
conglin <conglin8023@126.com>
2022-02-16 15:09:57 +0800

    add doxygen
    Signed-off-by: conglin <conglin8023@126.com>

--- docker/standard/build.sh
+++ docker/standard/build.sh
@@ -13,4 +13,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-docker build -t openharmony-docker-standard:0.0.7 .
+docker build -t openharmony-docker-standard:0.0.8 .
